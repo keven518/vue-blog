8f795d8ce922a778469ec62fee9a5b953dbdfe78
KEWEN <kewen@huazhu.com>
2018-08-31 20:08:05 +0800

    'init'

---
+++ src/page/Home.vue
@@ -0,0 +1,54 @@
+<!--  -->
+<template>
+  <div>
+    <!-- <index-swiper></index-swiper> -->
+    <ul class="kv-ui">
+      <li></li>
+      <li></li>
+      <li>asdfasdfas</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import IndexSwiper from '@/components/swiper/indexSwiper'
+export default {
+  name: '',
+
+  data () {
+    return {
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+
+  components: {
+    // IndexSwiper
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+.kv-ui{
+  background: chartreuse;
+  li{
+    color: #900;
+    font-size: 24px;
+  }
+}
+</style>
